d4371d3ddf937d729c2290f2d0ae80429316a1a8
Valentin Vesvalo <vesvalo@mail.ru>
2019-10-06 18:05:41 +0300

    try to output previously resized image from S3

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ exports.handler = function(event, _context, callback) {
     var dir = parts[1] || '';
     var options = parts[2].split('_');
     var filename = parts[3];
-    var putKey = dir + 'rsz/'+parts[2]+'/'+filename;
+    var resizedKey = dir + 'rsz/'+parts[2]+'/'+filename;
     console.log('Dir: '+dir+', File name: '+filename);
-    console.log('putKey: '+putKey);
+    console.log('resizedKey: '+resizedKey);
 
     var sizes = options[0].split("x");
     var action = options.length > 1 ? options[1] : null;
@@ -35,74 +35,99 @@ exports.handler = function(event, _context, callback) {
     }
 
     var contentType;
-    S3.getObject({Bucket: BUCKET, Key: dir + filename})
+    S3.getObject({Bucket: BUCKET, Key: resizedKey})
         .promise()
-        .then(data => {
-            contentType = data.ContentType;
-            var width = sizes[0] === 'AUTO' ? null : parseInt(sizes[0]);
-            var height = sizes[1] === 'AUTO' ? null : parseInt(sizes[1]);
-            var fit;
-            switch (action) {
-                case 'max':
-                    fit = 'inside';
-                    break;
-                case 'min':
-                    fit = 'outside';
-                    break
-                default:
-                    fit = 'cover';
-                    break;
-            }
-            var options = {
-                withoutEnlargement: true,
-                fit
-            };
-            return Sharp(data.Body)
-                .resize(width, height, options)
-                .rotate()
-                .toBuffer();
-        })
         .then(result => {
-            var pr = S3.putObject({
-                Body: result,
-                Bucket: BUCKET,
-                ContentType: contentType,
-                Key: putKey
-            }
-            //}).promise()
-            ).promise();
-            pr.then(function(data) {
-                console.log('Put Success');
-              }).catch(function(err) {
-                console.log(err);
-              });
-            const response = {
-                status: '200',
-                statusDescription: 'OK',
-                // headers: {
-                //     'cache-control': [{
-                //         key: 'Cache-Control',
-                //         value: 'max-age=100'
-                //     }],
-                //     'content-type': [{
-                //         key: 'Content-Type',
-                //         value: 'text/html'
-                //     }],
-                //     'content-encoding': [{
-                //         key: 'Content-Encoding',
-                //         value: 'UTF-8'
-                //     }],
-                // },
-                body: result.toString('base64'),
-                bodyEncoding: 'base64'
-            };
-            callback(null, response);
-        })
-        .catch(e => {
-            callback(null, {
-                statusCode: e.statusCode || 400,
-                body: 'Exception: ' + e.message,
-                headers: {"Content-Type": "text/plain"}
-            })
+                const response = {
+                    status: '200',
+                    statusDescription: 'OK',
+                    body: result.Body.toString('base64'),
+                    bodyEncoding: 'base64'
+                };
+                console.log("output from S3");
+                callback(null, response);
+            }, 
+            error => {
+                S3.getObject({Bucket: BUCKET, Key: dir + filename})
+                    .promise()
+                    .then(data => {
+                        contentType = data.ContentType;
+                        var width = sizes[0] === 'AUTO' ? null : parseInt(sizes[0]);
+                        var height = sizes[1] === 'AUTO' ? null : parseInt(sizes[1]);
+                        var fit;
+                        switch (action) {
+                            case 'max':
+                                fit = 'inside';
+                                break;
+                            case 'min':
+                                fit = 'outside';
+                                break
+                            default:
+                                fit = 'cover';
+                                break;
+                        }
+                        var options = {
+                            withoutEnlargement: true,
+                            fit
+                        };
+                        return Sharp(data.Body)
+                            .resize(width, height, options)
+                            .rotate()
+                            .toBuffer();
+                    })
+                    .then(result => {
+                        var pr = S3.putObject({
+                            Body: result,
+                            Bucket: BUCKET,
+                            ContentType: contentType,
+                            Key: resizedKey
+                        }
+                        //}).promise()
+                        ).promise();
+                        pr.then(function(data) {
+                            console.log('Put Success');
+                        }).catch(function(err) {
+                            console.log(err);
+                        });
+                        var response;
+                        if(result.length >= 1048576){
+                            console.log("response size > 1 MB");
+                            response = {
+                                statusCode: 301,
+                                headers: {"Location" : `https://cdn.pay.super.com${path}`}
+                            };
+                        }
+                        else {
+                            console.log("output from lambda");
+                            response = {
+                                status: '200',
+                                statusDescription: 'OK',
+                                // headers: {
+                                //     'cache-control': [{
+                                //         key: 'Cache-Control',
+                                //         value: 'max-age=100'
+                                //     }],
+                                //     'content-type': [{
+                                //         key: 'Content-Type',
+                                //         value: 'text/html'
+                                //     }],
+                                //     'content-encoding': [{
+                                //         key: 'Content-Encoding',
+                                //         value: 'UTF-8'
+                                //     }],
+                                // },
+                                body: result.toString('base64'),
+                                bodyEncoding: 'base64'
+                            };
+                        }                            
+                        callback(null, response);
+                    })
+                    .catch(e => {
+                        callback(null, {
+                            statusCode: e.statusCode || 400,
+                            body: 'Exception: ' + e.message,
+                            headers: {"Content-Type": "text/plain"}
+                        })
+                    });
         });
 }
